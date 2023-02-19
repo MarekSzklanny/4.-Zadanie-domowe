4a2c5635359d4ad680419c76f96e3194cb8997e7
MarekSzklanny <szklamarek@gmail.com>
2023-02-19 22:00:00 +0100

    Zadanie domowe z dnia nr 4.

---
+++ 4.-Zadanie-domowe/4.-Zadanie-domowe/Program.cs
@@ -0,0 +1,15 @@
+﻿var name = "Ewa";
+var gender = "Woman";
+var age = 33;
+if (age - 16 < 18 && gender == "Man")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
+else if (name == "Ewa" && age == 33 && gender == "Woman")
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (age - 4 < 30 && gender == "Woman" || name == "Ewa")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
